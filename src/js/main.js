90b3ccc47da180930bcebc3280bc7b65fdd5d5d9
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-11-18 16:19:02 -0600

    feat: añadir archivos html y javascript base

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+
+class Settings {
+    constructor() {
+        this._supabaseUrl = "https://ewxnxxuxozjziedzjvry.supabase.co";
+        this._supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV3eG54eHV4b3pqemllZHpqdnJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0ODgzNjcsImV4cCI6MjA3OTA2NDM2N30.ym9p2CyU4AwTip_ShPGdIxZo4hrFbWcSC_id3VoDB94";
+    }
+
+    get supabaseUrl() {
+        return this._supabaseUrl;
+    }
+
+    get supabaseKey() {
+        return this._supabaseKey;
+    }
+}
+
+function loadView() {
+    const settings = new Settings();   
+}
+
+window.addEventListener('load', loadView);
