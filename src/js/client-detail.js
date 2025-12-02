ce877eb8868722ea2c90c1c47ccaae0bc8e73733
Fernando Arturo Gómez Flores <fernando.gomez@blendwerk.com.mx>
2025-12-01 19:09:32 -0600

    feat: agregar detalles del producto en la vista de cliente

---
+++ src/js/client-detail.js
@@ -0,0 +1,25 @@
+function load() {
+    const vs = new ViewState();
+
+    var inventory = new InventoryManager();
+    const product = inventory.loadProduct(vs.state.selectedSku);
+
+    document.getElementById("product-sku-label").textContent = product.sku;
+    document.getElementById("product-name-label").textContent = product.name;
+    document.getElementById("category-label").textContent = product.category;
+    document.getElementById("manufacturer-label").textContent = product.manufacturer;
+    document.getElementById("price-label").textContent = parseFloat(product.price)
+        .toLocaleString("es-MX", {
+            style: "currency",
+            currency: "MXN"
+        });
+
+    document.getElementById("description-label").textContent = product.description;
+    document.getElementById("location-label").textContent = product.location;
+    document.getElementById("weight-label").textContent = product.weight;
+    document.getElementById("volume-label").textContent = product.volume;
+    document.getElementById("supplier-label").textContent = product.supplier;
+}
+
+
+window.addEventListener("load", load);
